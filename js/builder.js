1d7517e8cc4e5ce34d4649a0f8e446f70e5e4c2e
BrunoDaher <brunus9@gmail.com>
2021-10-09 01:46:20 -0300

    1st commit

---
+++ js/builder.js
@@ -0,0 +1,67 @@
+//abertura da pagina
+inicio();
+
+
+function inicio(){
+    var y = 'teste'
+    loadFrag('menu', 'header');    
+}
+
+function loadFrag(url,divName){
+    let path = url != "index"? "frags/":"";
+
+    //console.log(path + url + ".html");
+    fetch(path + url + ".html").
+        then( response =>   {
+        // The API call was successful!                
+            return response.ok ? response.text():false;            
+            })
+            .then( responseHtml =>  
+            {
+                loadHtml(responseHtml, divName, url);
+                // ^nesse momento o conteúdo já foi carregado                
+                if(url=='menu'){
+                    document.querySelector('.btnSumario').classList.remove('active');
+                }
+                else{
+                    sumario();
+                    document.querySelector('.btnSumario').classList.add('active');
+                } 
+            })
+            .catch(function (e) {        
+                console.log('Something went wrong. Descrição');
+                console.log(e);
+            });
+
+    setTimeout( ()=> { syncMenuInit(url)} , 100);       
+}
+
+
+function loadHtml(html,divName,url){
+    //document.querySelector('')
+
+    let _divTarget = document.querySelector(divName);                                
+        _divTarget.innerHTML = html;         
+        url=='menu' ? menuInit(): ""  ;    
+               
+}
+
+//loadFrag('inicio.html', 'main');
+function syncMenuInit(url){
+    //console.log('atualizando menus -> ' + url );
+    let link = document.querySelectorAll('.link');         
+    link ? link.forEach(item => addAction(item)):false;      
+}
+///attListener();
+
+function cria(tipo){
+    let obj = document.createElement(tipo);
+    return obj;
+}
+
+function criaComp(tipo,nomeClasse,id){
+    let obj = cria(tipo);
+    id ? obj.id = id:"";
+    obj.className = nomeClasse;
+    return obj;
+}
---
+++ js/menu.js
@@ -0,0 +1,80 @@
+function menuClear(){
+    let menu = document.querySelectorAll('.headMenu');
+        menu.forEach(item => item.classList.remove('active'));
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+
+
+function menuInit(){    
+    //monta um array com o seletor .headMenu    
+    let isHeadMenu = document.querySelectorAll('.headMenu');       
+
+       
+    if(isHeadMenu){
+        //let menu = document.querySelectorAll('.headMenu');
+        //itera o array e add uma action pra cada elemento
+        isHeadMenu.forEach(item => addAction(item));   
+    }
+}
+
+function toggle(div){
+    document.querySelector('.bar').classList.toggle('active');
+    document.querySelector('.' + div).classList.toggle('active');
+}
+
+function addAction(item){
+    //toggle('menuInner') ;
+ 
+    item.addEventListener('click', event => {
+        let t = event.target.className;
+        t == 'headMenu' ?toggle('menuInner'):"" ;
+        menuClear();
+        item.classList.add('active');
+        document.title = item.innerText;
+        document.querySelector('#currentItem').innerText = item.innerText;
+        loadFrag(item.getAttribute('_target'),'main');
+      })
+}
+
+function subMenuBox(str){
+    let obj =  criaComp('div','head ',false);
+    let a = cria('a');
+    a.append(str);    
+    str == 'Fechar' ? obj.addEventListener('click', toggleMenu):"";
+    obj.append(a);
+   
+    return obj;
+}
+
+function sumario(){
+    
+    //cria o menu
+    let submenu = criaComp('div','floatMenu','submenu');    
+    submenu.append(subMenuBox('Tópicos'));    
+    document.querySelector('main').prepend(submenu);
+
+    let menu = document.querySelectorAll('legend');        
+        menu.forEach(item => subMenuBuild(item, submenu));            
+    submenu.append(subMenuBox('Fechar'));    
+    //actions
+    document.querySelector('.btnSumario').addEventListener('click', toggleMenu);
+}
+
+function toggleMenu(){
+    let statsFloatMenu = document.querySelector('#submenu').style.display;
+            let isShow = statsFloatMenu != 'grid' ? false:true;     
+          let _isShow  = isShow ? 'none':'grid';
+    document.querySelector('#submenu').style.display = _isShow;
+    isShow = !isShow;
+}
+
+function subMenuBuild(item,submenu){
+    let _id = "nav_" + item.innerText.toLowerCase();
+    item.id= _id;
+    let div = cria('div');
+    let a = cria('a')
+    a.href = `#${_id}`;
+    a.append(item.innerText);
+    div.append(a);
+    submenu.appendChild(div);
+}
